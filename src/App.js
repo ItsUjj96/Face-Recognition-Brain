2c57f004bb8187e0b6a312e56e94a9349dcf1dc6
ItsUjj96 <ujjwaldhanfhole1@gmail.com>
2023-03-29 16:05:11 +0530

    f-3

--- src/App.js
+++ src/App.js
@@ -4,18 +4,34 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import { Component } from 'react';
 
 
-function App() {
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+onInputChange = (event) => {
+console.log(event.target.value);
+}
+
+onButtonSubmit = () => {
+  console.log('click')
+}
+render(){
   return (
     <div className="App">
      <Navigation/>
      <Logo/>
      <Rank />
-     <ImageLinkForm />
+     <ImageLinkForm onInputChange= {this.onInputChange} onButtonSubmit = {this.onButtonSubmit}/>
      {/* <FaceRecognition /> */}
     </div>
   );
+}
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,14 +1,16 @@
 import React from "react";
 import './ImageLinkForm.css'
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return (
         <div>
             <p>{'This magic brain will detect the faces from your image'}</p>
             <div className="center">
                 <div className="form center pa4 br3 shadow-5">
-                    <input type={Text} className="f4 pa2 w-70 center"></input>
-                    <button className="w-30 grow f4 link ph3 pv2 dib white bg-light">Detect</button>
+                    <input type="Text" className="f4 pa2 w-70 center" onChange={onInputChange}></input>
+                    <button className="w-30 grow f4 link ph3 pv2 dib white bg-light"
+                    onClick={onButtonSubmit}
+                    >Detect</button>
                 </div>
             </div>
         </div>
